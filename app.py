bb586d5f382ecdb408181620ef32d569c0f3e821
kendraq <79023202+kendraq@users.noreply.github.com>
2023-04-28 10:11:57 -0400

    changed f1app_helper to data_loaders added tabs

--- app.py
+++ app.py
@@ -5,12 +5,17 @@
 
 import datetime as dt
 import sqlite3
-import f1app_helpers as f1
+
+import fastf1
+
+# import f1app_helpers as f1
+from data_loaders import options_loader
 import dash_bootstrap_components as dbc
 import numpy as np
 import pandas as pd
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, State
 from dash_bootstrap_templates import ThemeSwitchAIO
+import dash_mantine_components as dmc
 
 dbc_css = (
     "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css"
@@ -19,22 +24,34 @@ dbc_css = (
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.PULSE, dbc_css])
 
+ol = options_loader()
 
-this_year = dt.date.today().year
-init_raceId = f1.get_init_raceId(this_year)
-init_event = f1.get_event(init_raceId)
-init_event_hdr = f1.get_event_header_str(init_event)
-init_event_table = f1.get_event_table(init_raceId)
-init_condensed_table = f1.condense_event_table(init_event_table)
-init_radio_button_options = f1.get_session_dict(init_event)
 
+this_year = ol.year    #dt.date.today().year
+init_raceId = ol.raceId    #f1.get_init_raceId(this_year)
+init_event = ol.event   #f1.get_event(init_raceId)
+init_event_hdr = ol.event_hdr   #f1.get_event_header_str(init_event)
+init_event_table = ol.event_table   #f1.get_event_table(init_raceId)
+init_condensed_table = ol.condensed_table   #f1.condense_event_table(init_event_table)
+init_radio_button_options = ol.radio_buttons   #f1.get_session_dict(init_event)
+
+
+current_year = dcc.Store(id='curr-year', data=this_year)
+current_round = dcc.Store(id='curr-round', data=1)
+current_race = dcc.Store(id='curr-race', data='R')
+
+def get_drivers_table(drivers):
+    return dbc.Col(html.H6(children=dbc.Table.from_dataframe(
+            drivers, striped=True,
+            bordered=True, hover=True
+            ), id='drivers'), className="m-6 dbc", md=10)
 
 header = dbc.Col(html.H3("Formula 1 Telemetry Visualization",
                          className="bg-primary text-white p-4 mb-2"), md=10)
 
 year_dropdown = dcc.Dropdown(
     id='year-dropdown',
-    options=f1.get_year_labels(2018, 2024),
+    options=ol.tm_years,
     value=str(this_year),
     multi=False,
     placeholder='Year'
@@ -47,10 +64,16 @@ event_dropdown = dcc.Dropdown(
     multi=False
 )
 
-placeholder = dbc.Col([dbc.Placeholder(color="info", className="me-1 mt-1 w-100")]
-)
+# placeholder = dbc.Col([dbc.Placeholder(color="info", className="me-1 mt-1 w-100")]
+# )
 
-button_group = html.Div(
+
+
+
+event_header = dbc.Col(html.H4(children=init_event_hdr, id='event-header',
+                               className="bg-info text-white p-4 mb-2"), md=10)
+
+button_group = dbc.Col(html.Div(
     [
         dbc.RadioItems(
             id="load-radios",
@@ -61,18 +84,13 @@ button_group = html.Div(
             options=init_radio_button_options,
             value='R'
         ),
-        dbc.Button(id="load-button", children='Load Race', value='R', color="primary", className="btn m-3"),
+        dbc.Button(id="load-button", children='Load Race', value='R',
+                   color="primary", className="btn m-3"),
     ],
     className="radio-group"
-)
+))
 
-# load_button = dbc.Col(dbc.Button(id="load-button", children='Load Race', value='R',
-#                    color="primary", className="me-1")),
 
-event_header = dbc.Col(html.H4
-                           (children=init_event_hdr, id='event-header',
-                               className="bg-info text-white p-4 mb-2"),
-                            md=10)
 event_table = dbc.Col(html.H6(children=dbc.Table.from_dataframe(
                         init_condensed_table, striped=True,
                         bordered=True, hover=True
@@ -80,49 +98,100 @@ event_table = dbc.Col(html.H6(children=dbc.Table.from_dataframe(
 )
 
 
+tab1_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H4('Add some visualizations', className="card-text"),
+            dbc.Col(get_drivers_table(pd.DataFrame([])), id='drivers')
+        ]
+    )
+)
 
-# button_group = html.Div(
-#     [
-#         dbc.RadioItems(
-#             id="load-radios",
-#             className="btn-group",
-#             inputClassName="btn-check",
-#             labelClassName="btn btn-outline-primary",
-#             labelCheckedClassName="active",
-#             options=init_radio_button_options,
-#             value='R',
-#         ),
-#         dbc.Button("Load Session", color="primary", className="me-1"),
-#     ],
-#     className="radio-group",
-# )
+tab2_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.P('Add some statistics', className="card-text")
+        ]
+    )
+)
+
+tabs = html.Div(
+    [
+        dbc.Tabs(
+            [
+                dbc.Tab(event_table, label='Table'),
+                dbc.Tab(tab1_content, label='Visualizations'),
+                dbc.Tab(tab2_content, label='Statistics')
+            ],
+            id='tabs', active_tab='Table'
+        ),
+        html.Div(id='content')
+    ]
+)
+
+
 
 @app.callback(
-    Output('event-dropdown', 'options'),
+    [Output('event-dropdown', 'options'),
+     Output('curr-year', 'data')],
     [Input('year-dropdown', 'value')]
 )
 
 def update_events(year):
-    return f1.get_event_labels(year)
+    return ol.get_event_labels(year), year
 
 @app.callback(
     [Output('event-header', 'children'),
-     Output('event-table', 'children')],
+     Output('event-table', 'children'),
+     Output('curr-round', 'data')],
     [Input('event-dropdown', 'value')]
 )
 
 def update_event_hdr(raceId):
-    event_df = f1.get_event(raceId)
-    event_header = f1.get_event_header_str(event_df)
-    event_table_expand = f1.get_event_table(raceId)
-    event_table_condensed = f1.condense_event_table(event_table_expand)
+    event_df = ol.get_event(raceId)
+    event_header = ol.get_event_header_str(event_df)
+    event_table_expand = ol.get_event_table(raceId)
+    event_table_condensed = ol.condense_event_table(event_table_expand)
     event_table = dbc.Table.from_dataframe(
                         event_table_condensed, striped=True,
                         bordered=True, hover=True
                         )
-    return event_header, event_table
-#
-# def update_selected_event(raceId):
+    round = event_df['Round']
+    print(round)
+    return event_header, event_table, round
+
+
+
+@app.callback(
+    [Output('drivers', 'value')],
+    [State('curr-year', 'data'),
+     State('curr-round', 'data'),
+     State('load-radios', 'value'),
+     Input('load-button', 'n_clicks')]
+)
+
+def load_session(year, round, race, nclicks):
+    if nclicks:
+        session = fastf1.get_session(int(year), int(round), race)
+        session.load()
+        drivers = pd.DataFrame(session.get_drivers())
+    else:
+        drivers = pd.DataFrame(['No Race Selected'])
+    return get_drivers_table(drivers)
+
+
+
+@app.callback(Output("content", "children"),
+              [Input("tabs", "active_tab")])
+def switch_tab(at):
+    if at == "Table":
+        return event_table
+    elif at == "Vizualizations":
+        return tab1_content
+    elif at == 'Statistics':
+        return tab2_content
+    return html.P("This shouldn't ever be displayed...")
+
 
 
 row1 = html.Div(
@@ -132,15 +201,24 @@ row1 = html.Div(
                 dbc.Col(year_dropdown, md=4),
                 dbc.Col(event_dropdown, md=6)
                 # dbc.Col(session_dropdown, md=4)
-            ]
-        ),
+            ]),
+        dbc.Row(
+            [
+                dbc.Col(event_header, className="md=10")
+            ]),
+        dbc.Row(
+            [
+                dbc.Col(button_group, className="w-40", align='left')
+            ], className="md=10",)
     ]
 )
 
 
 
+app.layout = dbc.Container([current_year, current_race, current_round,
+                            header, row1, tabs], fluid=True, className="m-4 dbc")
 
-app.layout = dbc.Container([header, row1, event_header, button_group, event_table], fluid=True, className="m-4 dbc")
+
 # @app.callback(
 #     # Output("pie-chart", "figure"),
 #     Output("year-dropdown", "value"),
--- data_loaders.py
+++ data_loaders.py
@@ -1,26 +1,33 @@
 import numpy as np
 import pandas as pd
 import sqlite3
+import datetime as dt
 
 
+DB = "data/f1db_lite.db"
+YEAR_START = 2018
 class options_loader():
 
-    def __init__(self, db, year_start, year_end):
-        self.db = db
-        self.year_start = year_start
-        self.year_end = year_end
+    def __init__(self):
+        self.db = DB
+        self.year_start = YEAR_START
         self.year = dt.date.today().year
         self.tm_years = self.set_tm_years()
-        self.race_ids = self.set_init_raceId()
-        self.event_labels = self.set_event_labels()
+        self.raceId = self.set_init_raceId()
+        self.event_labels = self.get_event_labels(self.year)
+        self.event = self.get_event(self.raceId)
+        self.event_hdr = self.get_event_header_str(self.event)
+        self.event_table = self.get_event_table(self.raceId)
+        self.condensed_table = self.condense_event_table(self.event_table)
+        self.radio_buttons = self.get_session_dict(self.event)
 
 
     def set_tm_years(self):
         tm_years = [{'label': year, 'value': year} for year in
-                    np.arange(self.year_start, self.year_end)]
+                    np.arange(self.year_start, self.year + 1)]
         return tm_years
 
-    def query(self, selection, table, where=condition):
+    def query(self, selection, table, condition):
         return f'select {selection} from {table} where {condition}'
 
 
@@ -34,12 +41,12 @@ class options_loader():
         return df
 
 
-    def set_raceId(self):
+    def set_init_raceId(self):
         query = self.query('raceId', 'races', '(year=? AND round=?)')
         raceId = self.exec_query(query, (self.year, 1)).values[0, 0]
         return raceId
 
-    def set_event_labels(self):
+    def get_event_labels(self, year):
         query = self.query('raceId, round, date, name', 'races', 'year=?')
         event_data_df = self.exec_query(query, params=(year,))
         event_data_df['label'] = (
@@ -50,7 +57,11 @@ class options_loader():
         event_data = event_data_df[['label', 'value']].to_dict('records')
         return event_data
 
-    def get_event(raceId):
+    def get_round(self, raceId):
+        query = self.query('round', 'races', f'raceId={raceId}')
+        round = self.exec_query(query, raceId).iat[0,0]
+
+    def get_event(self, raceId):
         query = self.query('*', 'races', 'raceID=?')
         current_race_df = self.exec_query(query, params=(str(raceId),))
         column_mapper = {'year': 'Season', 'round': 'Round', 'name': 'Event',
@@ -68,13 +79,13 @@ class options_loader():
         event_df = current_race_df.rename(column_mapper, axis=1).T[0].dropna()
         return event_df
 
-    def get_event_header_str(event_df):
+    def get_event_header_str(self, event_df):
         event_header_str = (f'Season {event_df["Season"]} ' +
                             f'Round {event_df["Round"]}: ' +
                             f'{event_df["Event"]}')
         return event_header_str
 
-    def get_session_dict(event_df):
+    def get_session_dict(self, event_df):
         current_sessions = [session[:-5] for session
                             in event_df.index
                             if session.endswith("Date")]
@@ -84,7 +95,7 @@ class options_loader():
                              for session in current_sessions]
         return session_dict_list
 
-    def get_event_table(raceId):
+    def get_event_table(self, raceId):
         result_list = (['driverId', 'constructorId', 'positionOrder',
                         'positionText', 'laps', 'grid', 'time', 'statusId'])
         driver_list = ['driverRef', 'number', 'code', 'forename', 'surname',
@@ -105,7 +116,7 @@ class options_loader():
         qu_results_wstatus = self.query_join('r.*, s.status',
                                              f'({qu_result_raceId}) r',
                                              'left',
-                                             'status s'
+                                             'status s',
                                              '(statusID)')
         qu_results_wst_dr = self.query_join(f'r.*, {driver_select}',
                                             f'({qu_results_wstatus}) r',
@@ -122,7 +133,7 @@ class options_loader():
             results_expand_df['surname'], sep=' ')
         return results_expand_df
 
-    def condense_event_table(results_expand_df):
+    def condense_event_table(self, results_expand_df):
         condensed_df = results_expand_df[['positionText', 'code', 'number',
                                           'laps', 'Driver', 'name', 'time',
                                           'status']]
